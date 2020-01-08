25abaa93e1d00877fb03d27837a211b09abe5b69
Anthony King <anthony.king@fundingoptions.com>
2020-01-08 11:31:53 +0000

    bluejay feedback: rely on serverless environments for controlling bootstrap
    This introduces SENTRY_DSN environment as a stub, as well as adds
    in an XRAY_ENABLED flag, to control when we load xray.
    
    This is needed, as XRAY does not run well while testing, and can't
    be reliably turned off with fixtures.
    
    The behaviour is this:
    - if SENTRY_DSN is not set, the Sentry client will internally not transmit
      anything.
    - if XRAY_ENABLED is not 'true', no patching will be performed, and the sdk
      will be disabled.
    
    SENTRY_DSN must be configured by the user.
    XRAY_ENABLED will be set if lambda tracing is enabled (default)

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/lambda_bootstrap.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/lambda_bootstrap.py
@@ -4,11 +4,13 @@
 # of the context.
 # You usually want to initialise clients here, such as X-Ray, Boto, or Sentry.
 
-import sentry_sdk
-from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
+import os
 
-from aws_xray_sdk.core import xray_recorder
+import aws_xray_sdk
+import sentry_sdk
 from aws_xray_sdk.core import patch_all as xray_patch_all
+from aws_xray_sdk.core import xray_recorder
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 
 def init_all(**kwargs):
@@ -18,13 +20,22 @@ def init_all(**kwargs):
 
 def init_sentry(sentry_dsn=None, sentry_integrations=None, **_):
     sentry_sdk.init(
-        sentry_sdk=sentry_dsn,
+        dsn=sentry_dsn,
         integrations=[AwsLambdaIntegration(), *(sentry_integrations or [])],
         environment=os.environ.get("APP_STAGE"),
     )
 
 
-def init_xray(xray_service_name=None, **_):
-    xray_service_name = xray_service_name or f"{os.environ.get("APP_NAME")} - {os.environ.get("APP_STAGE")}"
-    xray_recorder.configure(service=xray_service_name)
-    xray_patch_all()
+def init_xray(xray_service_name=None, xray_enabled=None, **_):
+    if xray_enabled is None:
+        xray_enabled = os.environ.get("XRAY_ENABLED", "") == "true"
+
+    if xray_enabled:
+        xray_service_name = (
+            xray_service_name
+            or f"{os.environ.get('APP_NAME')} - {os.environ.get('APP_STAGE')}"
+        )
+        xray_recorder.configure(service=xray_service_name)
+        xray_patch_all()
+    else:
+        aws_xray_sdk.global_sdk_config.set_sdk_enabled(False)
