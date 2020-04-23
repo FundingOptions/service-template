470ee472ba81ad671884267c22c29505154317d6
Anthony King <anthonydking@gmail.com>
2020-04-23 16:04:04 +0100

    separate sluggification into 2 modes, snakes and kebabs
    We have had issues where a multi-word service name generates everything with underscores.
    
    There are a few places where we want or need kebabs, so add support for both.

--- {{ cookiecutter.snake_name }}/{{ cookiecutter.snake_name }}/web/lambda_entry.py
+++ {{ cookiecutter.snake_name }}/{{ cookiecutter.snake_name }}/web/lambda_entry.py
@@ -1,7 +1,7 @@
 from mangum import Mangum
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
-from {{ cookiecutter.project_slug }} import lambda_boostrap
+from {{ cookiecutter.snake_name }} import lambda_boostrap
 
 from .app import app
 
